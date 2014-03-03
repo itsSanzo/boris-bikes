af2a2b4b52c8050480468d3465c1078d567f02fb
itsmurasaki <itsmurasaki@gmail.com>
2014-03-03 17:46:57 +0000

    more testing

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,6 +3,12 @@ require "bike.rb"
 describe Bike do
 	it "should not be broken after we create it" do
     bike = Bike.new
-    expect(bike.broken?).to be_false
+    expect(bike).not_to be_broken
+  end
+
+  it "should be able to break" do
+    bike = Bike.new
+    bike.break
+    expect(bike).to be_broken
   end
 end
