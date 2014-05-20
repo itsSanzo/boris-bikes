add29a1d345c262c8a8b26947f905578d2b0df34
itsmurasaki <itsmurasaki@gmail.com>
2014-05-20 22:10:00 +0100

    Garaga should release bikes when fixed

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,14 +1,23 @@
 require "garage"
+require "bike"
 
 describe Garage do
+
+	let(:bike) { Bike.new }
+	let(:garage) { Garage.new }
   
   it "should accept broken bikes" do
-  	garage = Garage.new
-  	bike = Bike.new
     expect(garage.bike_count).to eq(0)
     garage.accept(bike)
     expect(garage.bike_count).to eq(1)
   end 
+
+  it "should release a bike to the van when fixed" do
+    garage.accept(bike)
+    bike.fix
+    garage.release(bike)
+    expect(garage.bike_count).to eq(0)
+  end
 
 	
 end
